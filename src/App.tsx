8679ff0571a2860225e10e1f2fab894474a994ab
Andre <arc.xoc@gmail.com>
2022-04-19 21:08:39 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+
+import './App.css';
+
+//import the different pages from the pages folder
+//add your own pages here
+import { Home, About } from './Pages/';
+import { store } from './Store';
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default App;
